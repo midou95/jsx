d189efc2ebf4b3aa337bdda827afebc36042e09a
midou95 <midouchecha@gmail.com>
2020-11-27 22:51:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <div style={Border:'Solid 1px black'max-Width:'100vw'}>
+    </div>
+  );
+}
+
+export default App;
